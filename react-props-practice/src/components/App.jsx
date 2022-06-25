81cf4a78b7a4d8f38b7134ef3357f3dfffc292a9
Lilian <liliannzb@gmail.com>
2022-06-25 16:05:42 +0800

    react-map

--- react-props-practice/src/components/App.jsx
+++ react-props-practice/src/components/App.jsx
@@ -2,12 +2,27 @@ import React from "react";
 import Card from "./card";
 import contacts from "../contacts";
 
+function createCard(contact) {
+  return (
+    <Card
+      id={contact.id}
+      key={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      phone={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
 function App(props) {
   console.log(contacts);
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Card
+      {contacts.map(createCard)}
+
+      {/* <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
         phone={contacts[0].phone}
@@ -24,7 +39,7 @@ function App(props) {
         img={contacts[2].imgURL}
         phone={contacts[2].phone}
         email={contacts[2].email}
-      />
+      /> */}
     </div>
   );
 }
